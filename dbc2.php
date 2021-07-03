c79356d502241c662502471d968417bf58f9e171
TakumaInoue <inouetakuma19921228@gmail.com>
2021-07-03 14:28:09 +0900

    test_fetch

--- dbc2.php
+++ dbc2.php
@@ -14,9 +14,15 @@ try{
   // 2.SQLの実行
   $stmt = $dbh->query($sql);
   // 3.SQLの結果を受け取る
-  // 以下fetchAll
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+  // 以下fetchAll
+  // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  // public PDOStatement::fetchAll(int $mode = PDO::FETCH_DEFAULT): array
+
+  // 以下fetch
+  // public PDOStatement::fetch(int $mode = PDO::FETCH_DEFAULT, int $cursorOrientation = PDO::FETCH_ORI_NEXT, int $cursorOffset = 0): mixed
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  
   var_dump($result);
   $dbh = null;
 }catch(PDOException $e) { 
